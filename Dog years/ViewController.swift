afb1d35954d561f938305a8107cc6c8296a90090
blaedj <blaedj@gmail.com>
2015-04-15 06:38:31 -0500

    working app

--- Dog years/ViewController.swift
+++ Dog years/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageInput: UITextField!
+    @IBOutlet weak var outputYears: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    @IBAction func findButtonPressed(sender: AnyObject) {
+        let multiplier = 7 //: Int = Int(7)
+        if let years = self.ageInput.text?.toInt() {
+            outputYears.text = "Your dog is \(years * multiplier) years old"
+        }
+    }
 
 }
 
